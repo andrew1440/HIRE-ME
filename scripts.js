c97bda905864145336f0b97971394efc71011706
benson7-coder <bensonowino7@gmail.com>
2019-12-18 09:12:06 +0300

    added image display function

--- scripts.js
+++ scripts.js
@@ -58,43 +58,53 @@ function qnty() {
 }
 
 function ram() {
-    switch (parseInt(_f('dtls')[1].value)) {
+    switch (parseInt(_f('dtls')[2].value)) {
         case 1: return 0; break;
         case 2: return 2 * globVarA * 0.05; break;
         case 3: return 4 * globVarA * 0.05; break;
         case 4: return 8 * globVarA * 0.05; break;
         case 5: return 16 * globVarA * 0.05; break;
-        default: alert(_f('dtls')[1].value + 'invalid Selection Ram'); break;
+        default: alert(_f('dtls')[2].value + 'invalid Selection Ram'); break;
     }
 }
 function storage() {
-    switch (parseInt(_f('dtls')[2].value)) {
+    switch (parseInt(_f('dtls')[3].value)) {
         case 1: return 0; break;
         case 2: return 2 * globVarA * 0.1; break;
         case 3: return 4 * globVarA * 0.1; break;
         case 4: return 8 * globVarA * 0.1; break;
         case 5: return 16 * globVarA * 0.1; break;
-        default: alert(_f('dtls')[2].value + 'invalid Selection Storage'); break;
+        default: alert(_f('dtls')[3].value + 'invalid Selection Storage'); break;
     }
 }
 function cpu() {
-    switch (parseInt(_f('dtls')[3].value)) {
+    switch (parseInt(_f('dtls')[4].value)) {
         case 1: return 0; break;
         case 2: return 2 * globVarA * 0.2; break;
         case 3: return 4 * globVarA * 0.2; break;
         case 4: return 8 * globVarA * 0.2; break;
         case 5: return 16 * globVarA * 0.2; break;
-        default: alert(_f('dtls')[3].value + 'invalid Selection cpu'); break;
+        default: alert(_f('dtls')[4].value + 'invalid Selection cpu'); break;
     }
 }
 function display() {
-    switch (parseInt(_f('dtls')[4].value)) {
+    switch (parseInt(_f('dtls')[5].value)) {
         case 1: return 0; break;
         case 2: return 1 * globVarA * -0.1; break;
         case 3: return 2 * globVarA * 0.2; break;
         case 4: return 2 * globVarA * 0.1; break;
         case 5: return 4 * globVarA * 0.4; break;
         case 6: return 4 * globVarA * 0.3; break;
-        default: alert(_f('dtls')[4].value + 'invalid Selection display'); break;
+        default: alert(_f('dtls')[5].value + 'invalid Selection display'); break;
     }
 }
+// var image1 = new Image()
+//       image1.src=""  
+//       var image2 = new Image()
+//       image2.src=""
+//       var image3 = new Image()
+//       image3.src=""
+//       var image4 = new Image()
+//       image4.src=""
+//       var image5 = new Image()
+//       image5.src="images/img8.jpg"
